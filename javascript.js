fe0386eca7be0f626d797bc0e55f76b7c3244fe6
Davonne Vigil <davonnejv@gmail.com>
2023-03-29 02:18:55 -0500

    my personal website block9

---
+++ javascript.js
@@ -0,0 +1,25 @@
+//landing page javascript
+//prompt question to enter your name with an alert
+//changes h1 to a bigger size when hovered over
+//changes h1 back to normal
+//console logs "Hello World"
+
+let question = prompt("Hello! What is your name?");
+    alert("Hi, " + question + "!" + " " + "Welcome To Block 9 My Personal Website");
+
+//
+function changeSize() {
+    document.getElementById("change").style.fontSize = "5.5em";
+}
+
+changeSize();
+
+//
+function changeBack() {
+    document.getElementById("change").style.fontSize = "5em";
+}
+
+changeBack();
+
+//
+console.log("Hello World!");
---
+++ portfolio.js
@@ -0,0 +1,15 @@
+//function to switch image when mouse goes over image
+//function to switch image back when mouse goes out of image
+
+function switchPic() {
+    document.getElementById("gh").src="myGitHub.png";
+}
+
+switchPic();
+
+//
+function switchBack() {
+    document.getElementById("gh").src="github.jpg";
+}
+
+switchBack();
